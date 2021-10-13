04cc7d636e77d9fc1d1ad09f35a9f4cc4a8468a4
David Mc Ken <david.mcken@gmail.com>
2021-10-13 03:24:55 +0000

    Force backup to use complete inserts.

--- master/backup.sh
+++ master/backup.sh
@@ -3,4 +3,4 @@
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
 echo "Creating timestamp $DATE_WITH_TIME"
-docker-compose exec mysql sh -c 'mysqldump --all-databases --user=root --password=$MYSQL_ROOT_PASSWORD' | bzip2 > master-all-databases-$DATE_WITH_TIME.sql.bz2
+docker-compose exec mysql sh -c 'mysqldump --complete-insert --all-databases --user=root --password=$MYSQL_ROOT_PASSWORD' | bzip2 > master-all-databases-$DATE_WITH_TIME.sql.bz2
